b4f1a43fefb4299996f5c438980580a60bd67b61
SeeWoo Park <seewoopark46@gmail.com>
2025-07-18 01:14:11 +0900

    Initial commit for Render Deployment

--- database.py
+++ database.py
@@ -1,8 +1,14 @@
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-DATABASE_URL = "'postgresql+asyncpg://neondb_owner:npg_iV4tazvO0RBq@ep-mute-band-af56ugn1-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require'"
+load_dotenv()
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL not set")
+
+engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
